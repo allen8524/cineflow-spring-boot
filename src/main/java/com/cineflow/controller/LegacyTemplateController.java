142aa5c59f4639d1963c2862f198e02f3c593b88
황민서 <102010606+allen8524@users.noreply.github.com>
2026-04-30 11:35:40 +0900

    Harden legacy template routes with redirect and 404 policy

--- src/main/java/com/cineflow/controller/LegacyTemplateController.java
+++ src/main/java/com/cineflow/controller/LegacyTemplateController.java
@@ -1,28 +1,74 @@
 package com.cineflow.controller;
 
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 public class LegacyTemplateController {
 
-    private final ResourceLoader resourceLoader;
-
-    public LegacyTemplateController(ResourceLoader resourceLoader) {
-        this.resourceLoader = resourceLoader;
+    @GetMapping({
+            "/moviegrid.html",
+            "/moviegrid_light.html",
+            "/moviegridfw.html",
+            "/moviegridfw_light.html",
+            "/movielist_light.html"
+    })
+    public String redirectLegacyMovieListPages() {
+        return "redirect:/movies";
     }
 
-    @GetMapping("/{page:[a-zA-Z0-9_-]+}.html")
-    public String legacyHtmlPage(@PathVariable String page) {
-        Resource template = resourceLoader.getResource("classpath:/templates/" + page + ".html");
-        if (template.exists()) {
-            return page;
+    @GetMapping("/moviesingle_light.html")
+    public String redirectLegacyMovieDetailPage(@RequestParam(required = false) Long id) {
+        if (id != null) {
+            return "redirect:/movies/" + id;
         }
+        return "redirect:/movies";
+    }
+
+    @GetMapping({
+            "/homev2.html",
+            "/homev2_light.html",
+            "/homev3.html",
+            "/homev3_light.html",
+            "/index-2.html",
+            "/index_light.html",
+            "/landing.html",
+            "/comingsoon.html"
+    })
+    public String redirectLegacyHomePages() {
+        return "redirect:/";
+    }
+
+    @GetMapping({
+            "/blogdetail.html",
+            "/blogdetail_light.html",
+            "/bloggrid.html",
+            "/bloggrid_light.html",
+            "/bloglist.html",
+            "/bloglist_light.html",
+            "/celebritygrid01.html",
+            "/celebritygrid01_light.html",
+            "/celebritygrid02.html",
+            "/celebritygrid02_light.html",
+            "/celebritylist.html",
+            "/celebritylist_light.html",
+            "/celebritysingle.html",
+            "/celebritysingle_light.html",
+            "/seriessingle.html",
+            "/seriessingle_light.html",
+            "/userfavoritegrid.html",
+            "/userfavoritegrid_light.html",
+            "/userfavoritelist.html",
+            "/userfavoritelist_light.html",
+            "/userprofile.html",
+            "/userprofile_light.html",
+            "/userrate.html",
+            "/userrate_light.html"
+    })
+    public String blockUnsupportedLegacyPages() {
         throw new ResponseStatusException(HttpStatus.NOT_FOUND);
     }
 }
